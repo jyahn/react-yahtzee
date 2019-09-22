90efb77666cdab3410f8bc571424367c2019e0a0
Kieran Kay <kierankay@gmail.com>
2019-09-21 21:41:03 -0700

    added die graphics logic

--- src/Die.js
+++ src/Die.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import "./Die.css";
+import red1 from "./red-1.jpg"
+import red2 from "./red-2.jpg"
+import red3 from "./red-3.jpg"
+import red4 from "./red-4.jpg"
+import red5 from "./red-5.jpg"
+import red6 from "./red-6.jpg"
 
 class Die extends Component {
   constructor(props) {
@@ -15,12 +21,31 @@ class Die extends Component {
     return (
       <button
         className={this.props.locked ? "Die Die-locked" : "Die"}
-        onClick={this.handleToggleLocked}
-      >
-        {this.props.val}
+        onClick={this.handleToggleLocked}>
+        {(() => { switch (this.props.val) {
+          case 1:
+            return <img src={red1} style={{height: "100px", width: "100px"}}/>
+            break;
+          case 2:
+            return <img src={red2} style={{height: "100px", width: "100px"}}/>
+            break;
+          case 3:
+              return <img src={red3} style={{height: "100px", width: "100px"}}/>
+            break;
+          case 4:
+              return <img src={red4} style={{height: "100px", width: "100px"}}/>
+            break;
+          case 5:
+              return <img src={red5} style={{height: "100px", width: "100px"}}/>
+            break;
+          case 6:
+              return <img src={red6} style={{height: "100px", width: "100px"}}/>
+            break;
+            }
+          })()}
       </button>
     );
-  }
+    }
 }
 
 export default Die;
--- src/Game.js
+++ src/Game.js
@@ -12,11 +12,12 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameOver: false,
       dice: Array.from({
         length: NUM_DICE
-      }),
+      }).map((d, i) => Math.ceil(Math.random() * 6)),
       locked: Array(NUM_DICE).fill(false),
-      rollsLeft: NUM_ROLLS,
+      rollsLeft: NUM_ROLLS - 1,
       scores: {
         ones: undefined,
         twos: undefined,
@@ -36,6 +37,8 @@ class Game extends Component {
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
     this.toggleLocked = this.toggleLocked.bind(this);
+    this.restartGame = this.restartGame.bind(this);
+
   }
 
   roll(evt) {
@@ -61,10 +64,10 @@ class Game extends Component {
     }
   }
 
-  doScore(rulename, ruleFn) {
+  async doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
     if (this.state.scores[rulename] === undefined) {
-      this.setState(st => ({
+      await this.setState(st => ({
         scores: {
           ...st.scores,
           [rulename]: ruleFn(this.state.dice)
@@ -72,8 +75,38 @@ class Game extends Component {
         rollsLeft: NUM_ROLLS,
         locked: Array(NUM_DICE).fill(false),
       }));
-      this.roll();
+      if (!Object.values(this.state.scores).includes(undefined)) {
+        this.setState({gameOver: true, rollsLeft: 0, locked: [true, true, true, true, true]})
+      } else {
+        this.roll();
+      }
     }
+  }
+
+  restartGame() {
+    this.setState({
+      gameOver: false,
+      dice: Array.from({
+        length: NUM_DICE
+      }).map((d, i) => Math.ceil(Math.random() * 6)),
+      locked: Array(NUM_DICE).fill(false),
+      rollsLeft: NUM_ROLLS - 1,
+      scores: {
+        ones: undefined,
+        twos: undefined,
+        threes: undefined,
+        fours: undefined,
+        fives: undefined,
+        sixes: undefined,
+        threeOfKind: undefined,
+        fourOfKind: undefined,
+        fullHouse: undefined,
+        smallStraight: undefined,
+        largeStraight: undefined,
+        yahtzee: undefined,
+        chance: undefined
+      }
+    })
   }
 
   render() {
@@ -83,6 +116,14 @@ class Game extends Component {
         <button className="Game-reroll" disabled={this.state.locked.every(x => x)} onClick={this.roll} >
           {this.state.rollsLeft} Rerolls Left
           </button> <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+          <h1>Total Score: {Object.values(this.state.scores).reduce((red, score) => {
+            if (score) {
+              return red + score;
+            } else {
+              return red;
+            }
+          }, 0)}</h1>
+          {this.state.gameOver ? <button className="Game-reroll" onClick={this.restartGame}>Start Over</button> : null}
       </section >
     );
   }
